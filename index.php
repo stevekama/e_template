60ddb5ce15eecca278a1415c0b65dc2ff2ddefc3
stevekama <stevekamahertz@gmail.com>
2020-09-02 13:05:42 +0300

    introduction to ecommerce template

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="css/styles.css"/>
+    <title>E-Commerce template</title>
+</head>
+<body>
+    <div class="top-nav-bar">
+        <div class="search-box">
+            <img src="images/logo/logo.png" alt="Logo">
+            <input type="text" class="form-control">
+            <span class="input-group-text"></span>
+        </div>
+    </div>
+
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.4.4/umd/popper.min.js" integrity="sha512-eUQ9hGdLjBjY3F41CScH3UX+4JDSI9zXeroz7hJ+RteoCaY+GP/LDoM8AO+Pt+DRFw3nXqsjh9Zsts8hnYv8/A==" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
